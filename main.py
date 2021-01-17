2bb17cf4e197d6e9c60b14e6961fcdb54c3c31dc
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-17 22:47:07 +0000

    Nesting list and dictionaries.

--- main.py
+++ main.py
@@ -1,67 +1,33 @@
- ##Python Dictionaries
+# Nesting 
+capitals={
+  "France":"Paris",
+  "Germany":"Berlin",
 
-programming_dictionary = {
-  "Bug": "An error in a program that prevents the program from running as expected.", 
-  "Function": "A piece of code that you can easily call over and over again.",
 }
 
-#Retrieving items from dictionary.
-# print(programming_dictionary["Function"])
-
-#Adding new items to dictionary.
-programming_dictionary["Loop"] = "The action of doing something over and over again."
-
-#Create an empty dictionary.
-empty_dictionary = {}
-
-#Wipe an existing dictionary
-# programming_dictionary = {}
-# print(programming_dictionary)
-
-#Edit an item in a dictionary
-programming_dictionary["Bug"] = "A moth in your computer."
-# print(programming_dictionary)
-
-#Loop through a dictionary
-# for key in programming_dictionary:
-#   print(key)
-#   print(programming_dictionary[key])
-
-#######################################
-
-#Nesting 
-capitals = {
-  "France": "Paris",
-  "Germany": "Berlin",
-}
-
-#Nesting a List in a Dictionary
-
-travel_log = {
+# Nesting a list in a dictionary 
+travel_log={
   "France": ["Paris", "Lille", "Dijon"],
-  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
-}
-
-#Nesting Dictionary in a Dictionary
-
-travel_log = {
-  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
-}
-
-#Nesting Dictionary in a List
-
-travel_log = [
-  {
-    "country": "France", 
-    "cities_visited": ["Paris", "Lille", "Dijon"], 
-    "total_visits": 12
-  },
-  {
-    "country": "Germany", 
-    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
-    "total_visits": 5
+  "Germany": ["Berlin", "Hamburg", "Stutgart"],
+} 
+# Nest a Dictionary in Dictionary 
+travel_log={
+  "France":{"cities_visited":["Paris", "Lille", "Dijon"],"number_of_visits":12},
+  "Germany":{"cities_visited":["Berlin", "Hamburg", "Stutgart"],"number_of_visits":5},
+  
+} 
+# Nesting a dictionary in a list  
+travel_log=[
+ {
+   "Country": "France",
+ "cities_visited":["Paris", "Lille", "Dijon"],
+ "number_of_visits":12
+ },
+ {
+   "Country": "Germany",
+   "cities_visited":["Berlin", "Hamburg", "Stutgart"],"number_of_visits":5
   },
 ]
 
+print(travel_log)
 
